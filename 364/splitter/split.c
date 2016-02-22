218f5b8e225d72806c19a89fd7a86a465f92f4a1
Alex Clark <aclark@aclark.net>
2016-02-22 04:41:38 -0500

    Update

--- 364/splitter/split.c
+++ 364/splitter/split.c
@@ -3,48 +3,35 @@
 
 /*#define    L_tmpnam        25     */ /* (sizeof(P_tmpdir) + 15) */
 
-main( int argc, char *argv[])
-{
-  	FILE *infile;
-  	FILE *outfile;
-  	int i = 0;
-  	int j; 
-  	char s[L_tmpnam];	 /* (sizeof(P_tmpdir) + 15) */
- 
-  	if (argc == 2) 
-	{
-    		infile = fopen(argv[1],"r");
-    		if (infile == NULL) 
-		{
-      			printf("Unable to open the input file.\n");
-      			exit (-1);
-    		}
-    		else 
-		{
-      			printf("input from %s\n",argv[1]);
-    		}
-  	}
-  	else 
-	{
-    		infile = stdin;
-    		printf ("Input from stdin.\n");
-  	}
+main(int argc, char *argv[]) {
+  FILE *infile;
+  FILE *outfile;
+  int i = 0;
+  int j;
+  char s[L_tmpnam]; /* (sizeof(P_tmpdir) + 15) */
 
-  	outfile = fopen(tempnam(OUTFILE_DIR,s),"w");
-  	for (  ; (fscanf (infile, "%d", &j))!= EOF ;    ) 
-	{
-    
-  		if (i >= j) 
-		{  
-      			fclose(outfile);
-      			outfile = fopen(tempnam(OUTFILE_DIR, s), "w");
-    		}
-    
-    		fprintf(outfile, "%d\n", j);
-    		i = j; 
-  
-	    
-	}
+  if (argc == 2) {
+    infile = fopen(argv[1], "r");
+    if (infile == NULL) {
+      printf("Unable to open the input file.\n");
+      exit(-1);
+    } else {
+      printf("input from %s\n", argv[1]);
+    }
+  } else {
+    infile = stdin;
+    printf("Input from stdin.\n");
+  }
 
+  outfile = fopen(tempnam(OUTFILE_DIR, s), "w");
+  for (; (fscanf(infile, "%d", &j)) != EOF;) {
 
+    if (i >= j) {
+      fclose(outfile);
+      outfile = fopen(tempnam(OUTFILE_DIR, s), "w");
+    }
+
+    fprintf(outfile, "%d\n", j);
+    i = j;
+  }
 }
