aea0eb59b7bd4780f4505c75cf211d19e28b080d
Alex Clark <aclark@aclark.net>
2016-02-22 05:01:07 -0500

    Update

--- 364/tmp/tmpnam.c
+++ 364/tmp/tmpnam.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
-main(int argc, char *argv[])
-{
-  	char s[L_tmpnam];
-  	int i, j;
+main(int argc, char *argv[]) {
+  char s[L_tmpnam];
+  int i, j;
 
-  	if (argc != 2) 
-	{
-      		printf("usage: %s n\n", argv[0]);
-      		exit (-1);
-  	}
+  if (argc != 2) {
+    printf("usage: %s n\n", argv[0]);
+    exit(-1);
+  }
 
-  	j = atoi(argv[1]);
-  /*	printf("p_tmpdir is %s\n",P_tmpdir); */
-  	printf("p_tmpdir is %s\n","/home/aclark/classes/364/tmp"); 
-  	printf("L_tmpnam = %d \n",L_tmpnam);
-  	printf("TMP_MAX = %d \n",TMP_MAX);
- 
- /* 	for (i = 0 ; i <2* TMP_MAX -2; i++) tmpnam(s); */
+  j = atoi(argv[1]);
+  printf("p_tmpdir is %s\n", "./tmp");
+  printf("L_tmpnam = %d \n", L_tmpnam);
+  printf("TMP_MAX = %d \n", TMP_MAX);
 
-  	for (i=0; i < j; i++)
-    	{
-      	/*	tmpnam(s); */
-      		printf("%s \t", s); 
-      		printf("%s \n", s+9); 
-    	}
-   	exit(0); 
-}  
-
-
+  for (i = 0; i < j; i++) {
+    printf("%s \t", s);
+    printf("%s \n", s + 9);
+  }
+  exit(0);
+}
