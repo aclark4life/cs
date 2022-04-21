e87ad3c7bd284380f9552f45703c0b47adf8c7e9
Alex Clark <aclark@aclark.net>
2022-04-21 14:05:56 -0400

    Update

--- cs.mk
+++ cs.mk
@@ -1,9 +1,11 @@
 all:
 	cd 262; make all
 	cd 295; make all
+	cd 364/buffer; make
 clean:
 	cd 262; make clean
 	cd 295; make clean
+	cd 364/buffer; make clean
 black:
 	cd 262; make black
 	cd 295; make black
