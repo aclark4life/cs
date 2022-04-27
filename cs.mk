7af0f294cce4da3934f2ea6d2f0b5e17e44225ed
Alex Clark <aclark@aclark.net>
2022-04-27 14:35:36 -0400

    Update

--- cs.mk
+++ cs.mk
@@ -4,24 +4,28 @@ all:
 	cd 364/buffer; make
 	cd 364/ko; make
 	cd 364/merge; make
+	cd 364/rng; make
 clean:
 	cd 262; make clean
 	cd 295; make clean
 	cd 364/buffer; make clean
 	cd 364/ko; make clean
 	cd 364/merge; make clean
+	cd 364/rng; make clean
 format:
 	cd 262; make format
 	cd 295; make format
 	cd 364/buffer; make format
 	cd 364/ko; make format
 	cd 364/merge; make format
+	cd 364/rng; make format
 tidy:
 	cd 262; make tidy
 	cd 295; make tidy
 	cd 364/buffer; make tidy
 	cd 364/ko; make tidy
 	cd 364/merge; make tidy
+	cd 364/rng; make tidy
 
 avg:
 	262/avg 3
