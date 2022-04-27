7baf4b45cfc9c78aa3b1e83faf90588d91a96260
Alex Clark <aclark@aclark.net>
2022-04-27 14:13:08 -0400

    Update

--- cs.mk
+++ cs.mk
@@ -3,21 +3,25 @@ all:
 	cd 295; make all
 	cd 364/buffer; make
 	cd 364/ko; make
+	cd 364/merge; make
 clean:
 	cd 262; make clean
 	cd 295; make clean
 	cd 364/buffer; make clean
 	cd 364/ko; make clean
+	cd 364/merge; make clean
 format:
 	cd 262; make format
 	cd 295; make format
 	cd 364/buffer; make format
 	cd 364/ko; make format
+	cd 364/merge; make format
 tidy:
 	cd 262; make tidy
 	cd 295; make tidy
 	cd 364/buffer; make tidy
 	cd 364/ko; make tidy
+	cd 364/merge; make tidy
 
 avg:
 	262/avg 3
@@ -28,3 +32,5 @@ buffer:
 	cd 364/buffer; ./buffer input.txt
 ko:
 	364/ko/ko 5 5
+merge:
+	364/merge/merge
