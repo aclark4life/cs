85eaf7c0d7d8941747f20dc3decb0a1ec121197f
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-06-03 15:47:48 -0400

    Adjust logic

--- 999/arg.c
+++ 999/arg.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Number of arguments: %d\n", argc);
-    for(int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
+    if (argc == 1) {
+        printf("Number of arguments: 0\n");
+    } else {
+        printf("Number of arguments: %d\n", argc - 1);
+        for(int i = 1; i < argc; i++) {
+            printf("Argument %d: %s\n", i, argv[i]);
+        }
     }
     return 0;
 }
