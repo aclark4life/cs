752575df605b9de6cae5614c3c06f2f1e1e7f3d3
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-09-18 21:10:45 -0400

    Replace tokens.h with tokens2.h content as final version

--- 461/project/tok/tokens.h
+++ 461/project/tok/tokens.h
@@ -1,3 +1,20 @@
+From gfurr @csmaster.loyola.edu Wed Jan 29 19 : 39 : 02 1997 Return -
+    Path : <gfurr @csmaster.loyola.edu> Received
+    : from wisdom.loyola.edu by csmaster.loyola.edu(SMI - 8.6 / SMI -
+                                                    SVR4) id TAA03369;
+Wed, 29 Jan 1997 19 : 39 : 02 - 0500 Received
+    : by wisdom.loyola.edu(SMI - 8.6 / SMI - SVR4) id TAA02482;
+Wed,
+    29 Jan 1997 19 : 44 : 16 -
+        0500 From : gfurr @csmaster.loyola.edu(Gwennie Furr) Message -
+        Id : <199701300044.TAA02482 @wisdom.loyola.edu> Subject : tokens.h To
+    : aclark @csmaster(foo | sj) Date : Wed,
+    29 Jan 1997 19 : 44 : 15 - 0500(EST)X
+        - Mailer : ELM[version 2.4 PL25] MIME - Version : 1.0 Content -
+                   Type : text / plain;
+charset = US - ASCII Content - Transfer - Encoding : 7bit Content -
+          Length : 5877 Status : OR
+
 #define OPEN_PAREN_TOKEN 1    /* ( */
 #define CLOSE_PAREN_TOKEN 2   /* ) */
 #define OPEN_BRACE_TOKEN 3    /* { */
@@ -8,7 +25,7 @@
 #define COMMA_TOKEN 8         /* , */
 #define PERIOD_TOKEN 9        /* . */
 #define ASSIGNS_TOKEN 10      /* = */
-#define GREATER_TOKEN 11      /*  */
+#define GREATER_TOKEN 11      /* > */
 #define LESS_TOKEN 12         /* < */
 #define NOT_TOKEN 13          /* ! */
 #define TILDE_TOKEN 14        /* ~ */
@@ -16,7 +33,7 @@
 #define COLON_TOKEN 16        /* : */
 #define EQ_TOKEN 17           /* == */
 #define LE_TOKEN 18           /* <= */
-#define GE_TOKEN 19           /* = */
+#define GE_TOKEN 19           /* >= */
 #define NE_OP_TOKEN 20        /* != */
 #define LOGICAL_OR_TOKEN 21   /* || */
 #define LOGICAL_AND_TOKEN 22  /* && */
@@ -24,15 +41,14 @@
 #define DEC_TOKEN 24          /* -- */
 #define PLUS_TOKEN 25         /* + */
 #define MINUS_TOKEN 26        /* - */
-#define MUL_TOKEN 102         /* * */
 #define DIV_TOKEN 27          /* / */
 #define AND_TOKEN 28          /* & */
 #define OR_TOKEN 29           /* | */
 #define XOR_TOKEN 30          /* ^ */
 #define MOD_TOKEN 31          /* % */
 #define SHL_TOKEN 32          /* << */
-#define SAR_TOKEN 33          /*  */
-#define SHR_TOKEN 34          /*  */
+#define SAR_TOKEN 33          /* >> */
+#define SHR_TOKEN 34          /* >>> */
 #define ADD_ASSIGN_TOKEN 35   /* += */
 #define SUB_ASSIGN_TOKEN 36   /* -= */
 #define MUL_ASSIGN_TOKEN 37   /* *= */
@@ -42,8 +58,9 @@
 #define XOR_ASSIGN_TOKEN 41   /* ^= */
 #define MOD_ASSIGN_TOKEN 42   /* %= */
 #define SHL_ASSIGN_TOKEN 43   /* <<= */
-#define SAR_ASSIGN_TOKEN 44   /* = */
-#define SHR_ASSIGN_TOKEN 45   /* = */
+#define SAR_ASSIGN_TOKEN 44   /* >>= */
+#define SHR_ASSIGN_TOKEN 45   /* >>>= */
+
 #define ABSTRACT_TOKEN 46     /* abstract */
 #define BOOLEAN_TOKEN 47      /* boolean */
 #define BREAK_TOKEN 48        /* break */
@@ -100,3 +117,14 @@
 #define VOID_TOKEN 99         /* void */
 #define VOLATILE_TOKEN 100    /* volatile */
 #define WHILE_TOKEN 101       /* while */
+#define MUL_TOKEN 102         /* * */
+
+#define CONSTANT 103
+#define STRING_LITERAL 104
+#define IDENTIFIER 105
+#define ID_TOKEN 106
+#define FLOATING_POINT_LITERAL_TOKEN 107
+#define INTEGER_LITERAL_TOKEN 108
+#define BOOLEAN_LITERAL_TOKEN 109
+#define CHARACTER_LITERAL_TOKEN 110
+#define STRING_LITERAL_TOKEN 111
