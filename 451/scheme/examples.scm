bb197298318ce1d9d1b3e5b0fc67e39f8c3a8845
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-09-18 21:10:51 -0400

    Replace examples.scm with examples2.scm content as final version

--- 451/scheme/examples.scm
+++ 451/scheme/examples.scm
@@ -1,50 +1,65 @@
 ;
-; '~binkley/public_html/451/examples.scm' contains the following scheme functions.
+; '~binkley/public_html/451/examples2.scm' contains the following scheme examples
 ; Also see the file '~binkley/public_html/451/scheme.output'.
 ;
-
 (define nil '())   ; our version of scheme forgot to define 'nil'
 
-(define add (lambda (a b) (+ a b)))   ; or "(define (add a b) (+ a b)"
-(define sub (lambda (a b) (- a b)))   ; or "(define (sub a b) (- a b)"
-
-(define (last list)
-    (cond ((null? list) nil)
-          ((null? (cdr list)) (car list))
-	  (else (last (cdr list)))
+(define (length1 lst)			; a repeat
+    (cond ((null? lst) 0)
+	  (else (+ 1 (length1 (cdr lst))))
     )
 )
 
-(define (length x)
-    (cond ((null? x)  0)
-          (else  (+ 1 (length (cdr x))))
+;
+; for problem 7.2  			(DO PART a FOR HOMEWORK)
+;
+(define (length2 list result-so-far)
+    (cond  ((null? list) result-so-far)
+           (else (length2 (cdr list) (+ 1 result-so-far)))
     )
 )
 
-(define (append a b)
-    (cond  ((null? a) b)
-           (else (cons (car a) (append (cdr a) b)))
+;
+; problem 7.2c
+;
+(define (factorial1 n)			; linear recursive version
+    (cond ((eq? n 1) 1)
+	  (else (* n (factorial1 (- n 1))))
     )
 )
 
-(define (reverse x)
-    (cond  ((null? x) nil)
-           (else (append (reverse (cdr x)) (list (car x))))
+(define (factorial-helper n result)	; tail recursive version (helper)
+    (cond ((eq? n 0) result)
+          (else (factorial-helper (- n 1) (* result n)))
     )
 )
 
-(define (reverse2-helper in out)
-    (cond ((null? in) out)
-          (else (reverse2-helper (cdr in) (cons (car in) out)))
-    )
+(define (factorial2 x)			; tail recursive version
+    (factorial-helper x 1)
 )
 
-(define  (reverse2 x)
-    (reverse2-helper x nil)
+;
+; Scheme has First Class Functions.  Thus, function are values just like
+; integers, booleans, etc.. For example, the following function converts a
+; binary function into a unary one by supplying one argument.  Note that this
+; function takes a function as an argument and returns a function as its result.
+;
+(define (fix-first-argument bi-func constant)
+    (lambda (x) (bi-func constant x))
 )
 
-(define (map f list)
-    (cond  ((null? list) nil)
-           (else  (cons (f (car list)) (map f (cdr list))))
+;
+; For example, fix-first-argument is to define the function increment from
+; the function +.
+;
+(define increment (fix-first-argument + 1))
+
+;
+; Yet another factorial.  Unlike the other two this version produce a function
+; that computes the answer, rather than computing an integer value.
+;
+(define (fact n)
+    (cond  ((eq? n 1) 1)
+	   (else (list '* n (fact (- n 1))))
     )
 )
