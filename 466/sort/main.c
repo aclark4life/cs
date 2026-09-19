c1d9639a2eebb748b6687283a1547fbdc1eb0d00
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-09-18 21:11:23 -0400

    Replace main.c with 2.c content (version 2)

--- 466/sort/main.c
+++ 466/sort/main.c
@@ -7,26 +7,48 @@
 
 main() {
   int a[2];
-  int pid, fd, i;
+  int pid, i;
   char **tokenv = (char **)malloc(sizeof(char *));
-  char *buf = (char *)malloc(sizeof(char));
   FILE *infile;
+  char *s;
 
-  infile = fopen("main.c", "r");
-
-  for (i = 0; i < 50; i++)
-    buf[i] = NULL;
-
+  infile = fopen("test", "r");
   tokenv[0] = "sort";
   pipe(a);
 
   pid = fork();
   switch (pid) {
-  case 0: /*execvp(tokenv[0],tokenv);*/
+  case 0:
+    close(0);  /* close stdin */
+    dup(a[0]); /* 'execvp sort' to the pipe instead of stdin */
+    close(a[0]);
+    execvp(tokenv[0], tokenv); /* exec sort */
     break;
   default:
-    fgets(buf, 50, infile);
-    write(1, buf, 50);
+    close(1);  /* close stdout */
+    dup(a[1]); /* printf to the pipe instead of stdout */
+    close(a[1]);
+    for (i = 0; (s = fread_long(infile)) != NULL; i++)
+    /* while ((s=fread_long(infile))!=NULL) */
+    {
+      if (strlen(s) >= 50) {
+        tokenv = realloc(tokenv, sizeof(char *) * (i + 2));
+        tokenv[i] = s;
+        printf("%s\n", s);
+      }
+    }
+    /*
+            for(i=0;(s=fread_long(infile))!=NULL;i++)
+            {
+                    if (strlen(s) >= 50)
+                    {
+                            tokenv[i]=s;
+                            tokenv = realloc(tokenv,sizeof(char*)*(i+2));
+                            printf("%s\n",s);
+                    }
+            }
+    */
+
     break;
   }
 }
