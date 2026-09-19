98fd3498aafbddad67b90df66bbc9dd2dc361438
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-09-18 21:11:12 -0400

    Restore original stat.c content as the final polished version

--- 461/hw/stat/stat.c
+++ 461/hw/stat/stat.c
@@ -12,25 +12,14 @@ main(int argc, char *argv[]) {
   if (argc == 2) {
     infile = fopen(argv[1], "r");
     if (infile == NULL) {
-      printf("Unable to open blah blah\n");
+      printf("Unable to open input file.\n");
       exit(-1);
     }
   } else
     exit(-1);
   path = argv[1];
   stat(path, buf);
-
-  printf("st_mode>%d\n", buf->st_mode);
-  printf("st_ino>%d\n", buf->st_ino);
-  printf("st_dev>%d\n", buf->st_dev);
-  printf("st_rdev>%d\n", buf->st_rdev);
-  printf("st_nlink>%d\n", buf->st_nlink);
-  printf("st_uid>%d\n", buf->st_uid);
-  printf("st_gid>%d\n", buf->st_gid);
-  printf("st_size>%d\n", buf->st_size);
-  printf("st_atime>%d\n", buf->st_atime);
-  printf("st_mtime>%d\n", buf->st_mtime);
-  printf("st_ctime>%d\n", buf->st_ctime);
-
   buffer = (char *)malloc(sizeof(char) * (buf->st_size));
+  fread(buffer, sizeof(char), buf->st_size, infile);
+  printf("%s", buffer);
 }
