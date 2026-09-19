40cf6285e5310a046d0bba1d127f0be05ecabd1d
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-09-18 21:10:51 -0400

    Replace alloca.c with alloca2.c content as final version

--- 461/hw/alloca/alloca.c
+++ 461/hw/alloca/alloca.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-call_alloca(int x)
+call_alloca()
 
 {
-  x = (int)malloc(sizeof(int));
-  return (x);
+  int *x;
+  x = (int *)alloca(sizeof(int));
 }
 
 main(int argc, char *argv) {
-  int c;
   int i;
 
   for (i = 0; i < 1000000; i++) {
-
-    call_alloca(c);
+    call_alloca();
   }
 }
